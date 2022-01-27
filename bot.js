604697896c9cd57d37c5a3b275f4974c0b0ae3bd
Dino Gržinić <62692130+9helix@users.noreply.github.com>
2022-01-27 13:56:53 +0000

    Initial commit

---
+++ bot.js
@@ -0,0 +1,20 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const token = process.env['token'];
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply(`Pong! ${Math.round(client.ws.ping)} ms`);
+  } else if (interaction.commandName === 'video') {
+    await interaction.reply('https://www.youtube.com/playlist?list=UUIuFRHmDktSjlHex7Hahcug&playnext=1&index=1');
+  }
+});
+
+keepAlive()
+client.login(token);
